385ccbf84c076d2d1b959c162ef32cb3c81e3940
alexander.ng <alexanderng@Alexander-Ng.local>
2018-01-04 21:01:55 -0800

    starting

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// Initial setup and constants
+const Express = require('express');
+const app = express();
+const port = 3000;
+
+// app.use((err, req, res, next) => {
+//     console.log(`[${new Date()}] ${req}`);
+//     next();
+// });
+
+app.get('/',(req,res) => res.send('Hello World!'));
+
+app.listen(port,()=>console.log("And I'm up! Yay :P"));
