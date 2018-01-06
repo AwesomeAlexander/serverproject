e783a6194d8028971ffc10ee67713c6d1c914871
alexander.ng <alexanderng@Alexander-Ng.local>
2018-01-05 22:11:06 -0800

    stuffs

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ const port = 3000;
  * Utility function to be able to get the short readable timestamp of an event
  */
 Date.prototype.timestamp = function() {
-    return `[${this.toJSON().slice(0,10).replace(/-/g,'/')} ${this.getHours()}:${this.getMinutes()}:${this.getSeconds()}]`;
+	return `[${this.toJSON().slice(0,10).replace(/-/g,'/')} ${this.getHours()}:${this.getMinutes()}:${this.getSeconds()}]`;
 };
 
 /*
  * Logging functionality middleware
  */
 app.use((req, res, next) => {
-    console.log(`${new Date().timestamp()} Recieved ${req.method} request to '${req.url}'`);
-    next();
+	console.log(`${new Date().timestamp()} Recieved ${req.method} request to '${req.url}'`);
+	next();
 });
 
 // Routing
@@ -23,5 +23,5 @@ app.use('/',require(`${__dirname}/routing/page.js`));
 
 // Start App
 app.listen(port,()=>{
-    console.log(`Listening at 'http://localhost:${port}' on ${new Date().timestamp()}`);
+	console.log(`Listening at 'http://localhost:${port}' on ${new Date().timestamp()}`);
 });
