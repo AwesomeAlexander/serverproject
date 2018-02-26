c8de28e24dbc112cbfa5d991bbe18320ac8d28b8
alexander.ng <alexanderng@Alexander-Ng.local>
2018-02-26 13:49:24 -0800

    fixing js

--- src/router.js
+++ src/router.js
@@ -9,22 +9,18 @@ const fs = require('fs');
 const router = express.Router();
 
 // Routing here
-router.get('/',(req,res,next)=>{
+router.get('/',(req,res)=>{
 	res.sendFile(`${__dirname}/pages/index.html`);
-	next();
 });
 
-router.get('/pages/:page',(req,res,next) => {
-	console.log('hi');
+router.get('/:page',(req,res) => {
 	var pages = fs.readdirSync(path.join(__dirname,'/pages'),"utf8");
 	if (pages.indexOf(req.params.page) > -1) {
 		let pageFile = path.join(__dirname,'/pages',req.params.page),
 			pageFileDir = fs.readdirSync(pageFile,"utf8");
-			console.log(pageFile);
-			console.log(pageFileDir);
-			if (pageFileDir.indexOf('index.html') < 0) {
-			// No index.html in the file
-			res.send(`This file (${req.params.page}) does not have an index.html`);
+		if (!pageFileDir.find(f => f.match(/index\.\w+/))) {
+			// No index.* in the file
+			res.send(`The page requested ('${req.params.page}') does not have an index document`);
 		} else {
 			// Display page
 			res.sendFile(path.join(pageFile,'/index.html'));
@@ -32,7 +28,6 @@ router.get('/pages/:page',(req,res,next) => {
 	} else {
 		res.send(`The '${req.params.page}' page does not exist!`);
 	}
-	next();
 });
 
 // Exporting
